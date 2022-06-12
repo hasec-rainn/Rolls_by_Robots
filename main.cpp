a6ebb3833f72a0473242b8f3f0c00ebe2cea9415
SkyBoundCaptain <hasecrainn@gmail.com>
2022-06-12 12:54:27 -0700

    made some changes

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<cstring>
 #include<typeinfo>
+#include<cmath>
 
 using namespace std;
 
@@ -8,19 +9,23 @@ using namespace std;
 #define INF -1
 
 #define NDMGTYPE 13
+#define NEFFECT 23
+#define NATT 6
 
 // https://rpg.stackexchange.com/questions/70335/how-do-i-calculate-the-chance-to-hit-a-given-ac?newreg=7e3de3c84d9a42ba96b932f394793d7f
 
-
-char status[21][STRSIZE] =\
+char status[NEFFECT][STRSIZE] =\
                 {"None\0", "Blinded\0", "Charmed\0", "Deafened\0",\
                 "Exhaustion I\0", "Exhaustion II\0",  "Exhaustion III\0", "Exhaustion IV\0",\
-                "Exhaustion V\0", "Exhaustion VI\0","Frightened\0", "Grappled\0",\
-                "Incapacitated\0", "Invisible\0", "Paralyzed\0", "Petrified\0",\
+                "Exhaustion V\0", "Exhaustion VI\0","Flying\0","Frightened\0", "Grappled\0",\
+                "Incapacitated\0", "Invincible\0", "Invisible\0", "Paralyzed\0", "Petrified\0",\
                 "Poisoned\0", "Prone\0", "Restrained\0", "Stunned\0",\
                 "Unconscious\0"};
 
-char aType[4][STRSIZE] = {"MeleeAtk\0", "SpellAtk\0", "RangedAtk\0", "Buff\0"};
+char aType[][STRSIZE] = {"MeleeAtk\0", "SpellAtk\0", "RangedAtk\0",\
+                    "HealBuff\0", "SelfHealBuff\0", "ConditionBuff\0",\
+                    "SelfCondBuff\0","ConditionSave\0",\
+                    "DamageSave\0"};
 
 char dmgType[NDMGTYPE][STRSIZE] =\
                 {"Acid\0", "Bludgeoning\0", "Cold\0", "Fire\0",\
@@ -28,41 +33,41 @@ char dmgType[NDMGTYPE][STRSIZE] =\
                 "Poison\0", "Psychic\0", "Radiant\0", "Slashing\0",\
                 "Thunder\0"};
 
-short GetSType(char* item) {
+// short GetSType(char* item) {
 
-    short index;
-    for(index=0; index<21; index++) {
-        if (strcmp(status[index], item) == 0) {
-            return index;
-        }
-    }
+//     short index;
+//     for(index=0; index<NEFFECT; index++) {
+//         if (strcmp(status[index], item) == 0) {
+//             return index;
+//         }
+//     }
 
-    return -1;
-}
+//     return -1;
+// }
 
-short GetAType(char* item) {
+// short GetAType(char* item) {
 
-    short index;
-    for(index=0; index<3; index++) {
-        if (strcmp(aType[index], item) == 0) {
-            return index;
-        }
-    }
+//     short index;
+//     for(index=0; index<3; index++) {
+//         if (strcmp(aType[index], item) == 0) {
+//             return index;
+//         }
+//     }
 
-    return -1;
-}
+//     return -1;
+// }
 
-short GetDMGType(char* item) {
+// short GetDMGType(char* item) {
 
-    short index;
-    for(index=0; index<13; index++) {
-        if (strcmp(dmgType[index], item) == 0) {
-            return index;
-        }
-    }
+//     short index;
+//     for(index=0; index<13; index++) {
+//         if (strcmp(dmgType[index], item) == 0) {
+//             return index;
+//         }
+//     }
 
-    return -1;
-}
+//     return -1;
+// }
 
 
 class Action {
@@ -244,6 +249,115 @@ class ConditionSave : public Action {
         }
 };
 
+
+
+/*Action that heals a target character a certain amount*/
+class HealBuff : public Action {
+    protected:
+        short healing;
+
+    public:
+
+        char type[STRSIZE] = "HealBuff\0";
+
+        void Init(char* Name, short Range, short Healing) {
+            InitBase(Name,type,Range);
+
+            healing = Healing;
+        }
+
+        /*Displays the attributes of an Action object via printing to
+        stdout*/
+        void PrintAtt() {
+            PrintBase();
+            cout << "\n\tHealing Amount: " << healing;
+        }
+};
+
+
+
+/*Action that heals the character using this action a certain amount
+CANNOT BE USED ON OTHER CHARACTERS*/
+class SelfHealBuff : public Action {
+    protected:
+        short healing;
+
+    public:
+
+        char type[STRSIZE] = "SelfHealBuff\0";
+
+        void Init(char* Name, short Range, short Healing) {
+            InitBase(Name,type,Range);
+
+            healing = Healing;
+        }
+
+        /*Displays the attributes of an Action object via printing to
+        stdout*/
+        void PrintAtt() {
+            PrintBase();
+            cout << "\n\tHealing Amount: " << healing;
+        }
+};
+
+
+/*Action that gives a positive condition to a character*/
+class ConditionBuff : public Action {
+    protected:
+        char effect[STRSIZE];
+        short duration;
+
+    public:
+
+        char type[STRSIZE] = "ConditionBuff\0";
+
+        void Init(char* Name, short Range, char* Effect, short Duration) {
+            InitBase(Name,type,Range);
+
+            memcpy(effect, Effect,STRSIZE);
+            duration = Duration;
+        }
+
+        /*Displays the attributes of an Action object via printing to
+        stdout*/
+        void PrintAtt() {
+            PrintBase();
+            cout << "\n\tCondition: " << effect;
+            cout << "\n\tDuration: " << duration;
+        }
+};
+
+
+
+/*Action that gives a positive condition to the character that has
+this buff. CANNOT BE USED ON OTHER CHARACTERS*/
+class SelfCondBuff : public Action {
+    protected:
+        char effect[STRSIZE];
+        short duration;
+
+    public:
+
+        char type[STRSIZE] = "SelfCondBuff\0";
+
+        void Init(char* Name, short Range, char* Effect, short Duration) {
+            InitBase(Name,type,Range);
+
+            memcpy(effect, Effect,STRSIZE);
+            duration = Duration;
+        }
+
+        /*Displays the attributes of an Action object via printing to
+        stdout*/
+        void PrintAtt() {
+            PrintBase();
+            cout << "\n\tCondition: " << effect;
+            cout << "\n\tDuration: " << duration;
+        }
+};
+
+
+
 /********************************************************************
  * Class: Character
  * Desc: Character contains all the relevant information regarding a
@@ -259,20 +373,25 @@ class Character {
         short speed;
         //proficiency bonus is not included since it is included in actions
 
-        /*complicated, is it not? The dmgModArr is an array which details
+        /*The dmgModArr is an array which details
         how the character is afflicted by a certain damage type. Each index
         corresponds to to the corresponding damage type in the "dmgType"
         array.
         The value contained at a particular index describes how the character
         is impacted. Below are the possible values that may show up:
+
         0 -> character takes no damage (has immunity to damage source)
         -2 -> character takes half damage (has resistance to dmg source)
         1 -> character takes normal damage
         2 -> character takes double damage (has vulnerability to dmg source)*/
         short dmgModArr[NDMGTYPE];
 
-        char effects[21][STRSIZE];
-        short effectDurations[21];
+        /*Each index in "effects" corresponds to a specific status effect
+        (see the char[][] status at top of file.)
+        True 1 means that the particular effect is active, false means
+        the particular effect is not active.*/
+        bool effects[NEFFECT];
+        short effectDurations[NEFFECT];
 
         short strScore; short strMod;
         short dexScore; short dexMod;
@@ -280,19 +399,16 @@ class Character {
         short intScore; short intMod;
         short wisScore; short wisMod;
         short chaScore; short chaMod;
-
+        bool attAdv[NATT];  //true if adv for respective ability
+        bool attDis[NATT];  //true if disadv for respective ability
 
         short nAct; Action** actions; //an array of nAct-many Action*
 
     public:
 
         Character(short MaxHP, short HP, short AC, short Speed,\
-                  short* DmgModArr, short StrScore, short StrMod,\
-                  short DexScore, short DexMod,short ConScore,\
-                  short ConMod, short IntScore, short IntMod,\
-                  short WisScore, short WisMod,\
-                  short ChaScore, short ChaMod,\
-                  short NAct, Action** Actions)
+                  short* DmgModArr, bool* Effects, short* scoreArr, short* modArr,\
+                  Action** Actions, short NAct)
         {
             maxHP = MaxHP; hp = HP;
             ac = AC;
@@ -300,17 +416,21 @@ class Character {
             for(int i=0; i<NDMGTYPE; i++) {
                 dmgModArr[i] = DmgModArr[i];
             }
+            strScore = scoreArr[0]; strMod = modArr[0];
+            dexScore = scoreArr[1]; dexMod = modArr[1];
+            conScore = scoreArr[2]; conMod = modArr[2];
+            intScore = scoreArr[3]; intMod = modArr[3];
+            wisScore = scoreArr[4]; wisMod = modArr[4];
+            chaScore = scoreArr[5]; chaMod = modArr[5];
 
-            strScore = StrScore; strMod = StrMod;
-            dexScore = DexScore; dexMod = dexMod;
-            conScore = ConScore; conMod = ConMod;
-            intScore = IntScore; intMod = IntMod;
-            wisScore = WisScore; wisMod = WisMod;
-            chaScore = ChaScore; chaMod = ChaMod;
+            for(int i=0; i<NEFFECT; i++) {
+                effects[i] = Effects[i];
+            }
 
             nAct = NAct; actions = Actions;
         }
 
+        /*Print everything you'd ever want to know about the character*/
         void PrintAtt() {
             cout << "\n###### Character " << name << " Attributes ######";
             cout << "\n\tMax HP: " << maxHP;
@@ -321,12 +441,19 @@ class Character {
             for(int i=0; i<NDMGTYPE; i++) {
                 cout << "\n\t\tDmgType[" << i << "]:" << dmgType[i] << ", mod: " << dmgModArr[i];
             }
+            cout << "\n\tActive effects:";
+            for(int i=0; i<NEFFECT; i++) {
+                cout << "\n\t\teffect " << status[i] << ": " << effects[i];
+            }
+
             cout << "\n\tSTR score: " << strScore << " (Mod: " << strMod << ")";
             cout << "\n\tDEX score: " << dexScore << " (Mod: " << dexMod << ")";
             cout << "\n\tCON score: " << conScore << " (Mod: " << conMod << ")";
             cout << "\n\tINT score: " << intScore << " (Mod: " << intMod << ")";
             cout << "\n\tWIS score: " << wisScore << " (Mod: " << wisMod << ")";
             cout << "\n\tCHA score: " << chaScore << " (Mod: " << chaMod << ")";
+            
+            cout << "\n\tAction types:";
             for(int i=0; i<nAct; i++) {
                 cout << "\n\t\tAction[" << i << "] type: " << actions[i]->type;
             }
@@ -339,41 +466,80 @@ class Character {
         short GetSpeed() {return speed;}
 
         /***********************************************************
-         * Function: ApplyAction
-         * Parameters: Action* a
+         * Function: ApplyAction(MeleeAtk* action, short hitMod, short dmgMod)
          * 
-         * Desc: Applies Action at index "a" to this character. 
-         *       If the action is an attack, the character may lose 
+         * Desc: Applies a particular action to this character.
+         *       The action may come from another character, or
+         *       it could potentially come from this character.
+         *  
+         *       Since this action is an attack, the character may lose 
          *       health and/or receive a negative effect.
-         * 
-         *       If the action is a buff/heal, the character may gain
-         *       health or receive a positive effect.
-         * 
-         * Example1: ApplyAction(fireball) -> character loses health
-         *           due to fire damage
-         * Example2: ApplyAction(healing_word) -> character gains
-         *           health
         ***********************************************************/
-        void ApplyAction(short a) {
-            if( strcmp(actions[a]->type, "MeleeAtk\0") == 0 ) {
-
-                cout << "\nlooks like this is a melee attack";
-
-            } else if ( strcmp(actions[a]->type, "RangedAtk\0") == 0 ) {
-
-                cout << "\nlooks like this is a ranged attack.";
-
-            } else if ( strcmp(actions[a]->type, "ConditionSave\0") == 0 ) {
-
-                cout << "\nlooks like this is a condition save.";
-
-            }
+        void ApplyAction(MeleeAtk* a, short hitMod, short dmgMod) {
+            cout << "\nlooks like this is a melee attack";
             return;
         }
 
+        /***********************************************************
+         * Function: ApplyAction(RangedAtk* action, short hitMod, short dmgMod)
+         * 
+         * Desc: Applies a particular action to this character.
+         *       The action may come from another character, or
+         *       it could potentially come from this character.
+         *  
+         *       Since this action is an attack, the character may lose 
+         *       health and/or receive a negative effect.
+        ***********************************************************/
+        void ApplyAction(RangedAtk* a, short hitMod, short dmgMod) {
+            cout << "\nlooks like this is a ranged attack";
+            return;
+        }
+
+        /***********************************************************
+         * Function: ApplyAction(DamageSave* action, short dmgMod)
+         * 
+         * Desc: Applies a particular action to this character.
+         *       The action may come from another character, or
+         *       it could potentially come from this character.
+         *  
+         *       Since this action is an attack, the character may lose 
+         *       health and/or receive a negative effect.
+        ***********************************************************/
+        void ApplyAction(DamageSave* a, short dmgMod) {
+            cout << "\nlooks like this is a Damage Save";
+            return;
+        }
+
+        /***********************************************************
+         * Function: ApplyAction(ConditionSave* action)
+         * 
+         * Desc: Applies a particular action to this character.
+         *       The action may come from another character, or
+         *       it could potentially come from this character.
+         *  
+         *       Since this action is an attack, the character may lose 
+         *       health and/or receive a negative effect.
+        ***********************************************************/
+        void ApplyAction(ConditionSave* a) {
+            cout << "\nlooks like this is a Condition Save";
+            return;
+        }
+
+        /***********************************************************
+         * Function: ApplyAction(HealBuff* action, short hitMod, short healMod)
+         * 
+         * Desc: Applies a particular action to this character.
+         *       The action may come from another character, or
+         *       it could potentially come from this character.
+         *  
+         *       Since this action is an buff, the character may recieve 
+         *       health and/or receive a positive effect.
+        ***********************************************************/
+        void ApplyAction(HealBuff* a, short hitMod, short healMod) {
+            cout << "\nlooks like this is a Condition Save";
+            return;
+        }
 };
-
-
 
 
 int main() {
@@ -382,24 +548,31 @@ int main() {
     RangedAtk axeThrow; axeThrow.Init("Axe Throw\0", 8, 120, 12, "Piercing\0");
     DamageSave bearHug; bearHug.Init("Bear Hug\0",5,20,"Bludgeoning\0",14,"Dex\0",false);
     ConditionSave knockout; knockout.Init("Knockout Punch\0",5,"Incapacitated\0",2,14,"Con\0"); 
-    
-    short nAct = 4;
-    Action* atkPtrArr[4];
+    HealBuff goodBerry; goodBerry.Init("Good Berry\0",5, 1);
+    SelfCondBuff rage;
+
+
+    short scores[] = {18,16,16,8,10,7};
+    short mods[] = {4,3,3,-1,0,-2};
+    bool effects[NEFFECT] = {}; 
+
+    short nAct = 6;
+    Action* atkPtrArr[6];
     atkPtrArr[0] = &clubStrike;
     atkPtrArr[1] = &axeThrow;
     atkPtrArr[2] = &bearHug;
     atkPtrArr[3] = &knockout;
 
 
-    short tDmgModArr[NDMGTYPE] = {1,1,1,1,1,1,1,1,1,1,1,1,1};
+    short dmgModArr[NDMGTYPE] = {1,1,1,1,1,1,1,1,1,1,1,1,1};
 
-    for(int i=0; i<4; i++) {
-        atkPtrArr[i]->PrintAtt();
-    }
+    // for(int i=0; i<4; i++) {
+    //     atkPtrArr[i]->PrintAtt();
+    // }
 
-    Character thageth = {65,65,16,30,tDmgModArr,18,4,14,2,16,3,8,-1,10,1,7,-2,nAct,atkPtrArr};
+    Character thageth = {65,65,16,30,dmgModArr,effects,scores,mods,atkPtrArr,nAct};
 
-    thageth.PrintAtt()
+    thageth.ApplyAction(&knockout);
 
-    return 1;
+    return 0;
 }
